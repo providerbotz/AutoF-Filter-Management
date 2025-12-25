2e9bb240af5505633c060eb875cdcfeea974882d
˹  ᴘʀᴏᴠɪᴅᴇʀʙᴏᴛᴢ  ˼ <animeokids@gmail.com>
2025-12-25 15:11:05 +0530

    Delete Prividerbotz directory

--- Prividerbotz/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import Database, \
-    save_file, get_file_details, Media, get_search_results
-
-db = Database()
--- Prividerbotz/functions/_init_.py
+++
@@ -1,2 +0,0 @@
-from .handlers import Manual
-from .media_details import humanbytes
